fe869d402feac38096de8523bd206ae4aa9f4da2
unknown <HFPing>
2018-11-25 15:37:15 -0600

    feat: Error component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,18 @@ import { CssBaseline } from '@material-ui/core';
 import Home from './screens/Home/Home';
 import Plots from './screens/Plots/Plots';
 
+const Error = () => (
+  <div>
+    <p>Path does not exist</p>
+  </div>
+)
+
 const App = () => (
   <BrowserRouter>
     <MuiThemeProvider>
       <Route exact path="/" component={Home} />
-      <Route exact path="/plots" component={Plots} />
+      <Route path="/plots" component={Plots} />
+      <Route component={Error} />
     </MuiThemeProvider>
   </BrowserRouter>
 );
