55f375f49a4f09ee45f9845ded9440ae41c8fa8e
Hugo <dummy@mio.com>
2018-11-27 07:14:08 -0600

    feat: Sin plot

--- src/screens/Plots/Tabs/Example9.jsx
+++ src/screens/Plots/Tabs/Example9.jsx
@@ -25,19 +25,19 @@ class Example9 extends PureComponent {
 
     // 5. X scale will use the index of our data
     var xScale = d3.scaleLinear()
-        .domain([0, n-1]) // input
-        .range([0, width]); // output
+      .domain([0, 2 * Math.PI]) // input
+      .range([0, width]); // output
 
     // 6. Y scale will use the randomly generate number 
     var yScale = d3.scaleLinear()
-        .domain([-1, 1]) // input 
-        .range([height, 0]); // output 
+      .domain([-1, 1]) // input 
+      .range([height, 0]); // output 
 
     // 7. d3's line generator
     var line = d3.line()
-        .x(function(d, i) { return xScale(i); }) // set the x values for the line generator
-        .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
-        .curve(d3.curveMonotoneX) // apply smoothing to the line
+      .x(function(d, i) { return xScale(2 * Math.PI * i / n); }) // set the x values for the line generator
+      .y(function(d) { return yScale(d.y); }) // set the y values for the line generator 
+      .curve(d3.curveMonotoneX) // apply smoothing to the line
 
     // 8. An array of objects of length N. Each object has key -> value pair, the key being "y" and the value is a random number
     var dataset = d3.range(n).map(function(d) { return {"y": Math.sin(2 * Math.PI * d / n) } })
@@ -51,27 +51,27 @@ class Example9 extends PureComponent {
 
     // 3. Call the x axis in a group tag
     svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(xScale)); // Create an axis component with d3.axisBottom
 
     // 4. Call the y axis in a group tag
     svg.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(yScale)); // Create an axis component with d3.axisLeft
+      .attr("class", "y axis")
+      .call(d3.axisLeft(yScale).tickSizeInner(-width)); // Create an axis component with d3.axisLeft
 
     // 9. Append the path, bind the data, and call the line generator 
     svg.append("path")
-        .datum(dataset) // 10. Binds data to the line 
-        .attr("class", "line") // Assign a class for styling 
-        .attr("d", line); // 11. Calls the line generator 
+      .datum(dataset) // 10. Binds data to the line 
+      .attr("class", "line") // Assign a class for styling 
+      .attr("d", line); // 11. Calls the line generator 
 
     // 12. Appends a circle for each datapoint 
     svg.selectAll(".dot")
         .data(dataset)
       .enter().append("circle") // Uses the enter().append() method
         .attr("class", "dot") // Assign a class for styling
-        .attr("cx", function(d, i) { return xScale(i) })
+        .attr("cx", function(d, i) { return xScale(2 * Math.PI * i / n) })
         .attr("cy", function(d) { return yScale(d.y) })
         .attr("r", 5);
   }
