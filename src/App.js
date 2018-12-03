740c1aab8a683e3140d695f4e6b643f08e44bce7
Hugo <dummy@mio.com>
2018-12-03 15:52:26 -0600

    feat: Routing better implemented

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { CssBaseline } from "@material-ui/core";
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { withStyles } from '@material-ui/core/styles';
-import { CssBaseline } from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import { blue, green } from "@material-ui/core/colors";
 
 import Home from './screens/Home/Home';
@@ -33,79 +26,23 @@ const theme = createMuiTheme({
   shape: { borderRadius: 4 }
 });
 
-/*const Error = () => (
+const ErrorScreen = () => (
   <div>
     <p>Path does not exist</p>
   </div>
-)*/
-
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
-  },
-  appBarBottom: {
-    top: 'auto',
-    bottom: 0,
-  },
-  toolbar: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-  },
-};
-
-const Header = ({ classes }) => (
-  <AppBar position="relative">
-    <CssBaseline />
-    <Toolbar>
-      <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
-        <MenuIcon />
-      </IconButton>
-      <Typography variant="h6" color="inherit" className={classes.grow}>
-        React Demo
-      </Typography>
-      <Button color="inherit">Login</Button>
-    </Toolbar>
-  </AppBar>
-);
-
-/*const BottomBar = ({ classes }) => (
-  <AppBar position="absolute" color="primary" className={classes.appBarBottom}>
-    <Toolbar className={classes.toolbar}>
-      <IconButton color="inherit" aria-label="Open drawer">
-        <MenuIcon />
-      </IconButton>
-      <div>
-        <IconButton color="inherit">
-          <SearchIcon />
-        </IconButton>
-        <IconButton color="inherit">
-          <MoreIcon />
-        </IconButton>
-      </div>
-    </Toolbar>
-  </AppBar>
-);
-*/
+)
 
 const App = ({ classes }) => (
   <BrowserRouter>
     <MuiThemeProvider theme={theme}>
-      <Header classes={classes} />
-      <div style={{ }}>
-        <Route exact path="/home" component={Home} />
-        <Route path="/" component={Plots} />
-        {/*<Route component={Error} />*/}
-        {/*<BottomBar classes={classes} />*/}
-      </div>
+      <CssBaseline />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/plots" component={Plots} />
+        <Route path="/*" component={ErrorScreen} />
+      </Switch>
     </MuiThemeProvider>
   </BrowserRouter>
 );
 
-export default withStyles(styles)(App);
+export default App;
