e1147409569219396a6ec3bf7e24296e8dda6f5f
Hugo <dummy@mio.com>
2018-12-04 16:27:53 -0600

    feat: Minor changes

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React, { PureComponent } from 'react';
 import { CssBaseline } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton
+} from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import MoreIcon from '@material-ui/icons/MoreVert';
