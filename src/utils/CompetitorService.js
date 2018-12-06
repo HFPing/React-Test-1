166425341526bfdd97ea36bfda781419397e9d3d
Hugo <dummy@mio.com>
2018-12-06 15:03:34 -0600

    fix: https endpoints for gh deployment

--- src/utils/CompetitorService.js
+++ src/utils/CompetitorService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 function getCompetitorsList() {
   return axios({
     method: 'get',
-    url: 'http://www.mocky.io/v2/5c0952d12f00007d41637da2',
+    url: 'https://www.mocky.io/v2/5c0952d12f00007d41637da2',
     timeout: 4000,
   })
     .then(res => {
