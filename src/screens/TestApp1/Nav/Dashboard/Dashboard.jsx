6db05c7ef708b2549e74d8824abb7fc54d85996a
Hugo <dummy@mio.com>
2018-12-06 14:48:34 -0600

    feat: Data loading UI handing

--- src/screens/TestApp1/Nav/Dashboard/Dashboard.jsx
+++ src/screens/TestApp1/Nav/Dashboard/Dashboard.jsx
@@ -1,32 +1,71 @@
 import React, { PureComponent } from 'react';
-import { connect } from 'react-redux'
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { connect } from 'react-redux';
 import { withStyles } from '@material-ui/core/styles';
 import {
   Typography,
   IconButton,
+  CircularProgress,
 } from '@material-ui/core';
+
+import { ACTIONS, TYPES } from '../../../../redux/actions';
+
+const Div = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
 
 const styles = theme => ({
   root: {
-    backgroundCoor: '#EEE',
+  },
+  progress: {
+    margin: theme.spacing.unit * 2,
   },
 });
 
 class Dashboard extends PureComponent {
+  componentDidMount() {
+    const { dispatch, system } = this.props;
+    if (!system[TYPES.LISTS_DOWNLOADED]) {
+      dispatch(ACTIONS.initDashboard());
+    }
+  }
+
   render() {
-    const { classes, dispatch } = this.props;
+    const { classes, system } = this.props;
     console.log(this.props);
 
+    if (system.loading) {
+      return (
+        <Div className={classes.root}>
+          <CircularProgress className={classes.progress} color="secondary" size={150} />
+          <Typography variant="h5" color="inherit">
+            {system.type}
+          </Typography>
+        </Div>
+      );
+    }
     return (
-      <div className={classes.root}>
+      <Div className={classes.root}>
         Test
-      </div>
+      </Div>
     );
   }
 }
 
+Dashboard.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+  system: PropTypes.shape({}).isRequired,
+};
+
 const mapStateToProps = (state) => {
-  return {};
+  const { system, lists } = state;
+  return { system, lists };
 };
 
 export default connect(mapStateToProps)(withStyles(styles)(Dashboard));
