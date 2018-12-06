751a2eddb24900a90a2a840dec7553eac5409cb0
Hugo <dummy@mio.com>
2018-12-06 15:25:57 -0600

    fix: Typo

--- src/utils/ShoppingListsService.js
+++ src/utils/ShoppingListsService.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 function getShoppinglistsList() {
   return axios({
     method: 'get',
-    url: 'https://www.mocky.io/v2/5c0965e12f00007b38637e18?mocky-delay=3000ms',
+    url: 'https://www.mocky.io/v2/5c0965e12f00007b38637e18',
     timeout: 4000,
   })
     .then(res => {
