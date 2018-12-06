f84be0497ce9eb1da15e8ee87c04262e94e2df3a
Hugo <dummy@mio.com>
2018-12-06 14:45:04 -0600

    feat: Theme modified and linting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import { BrowserRouter, Switch } from 'react-router-dom';
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline } from '@material-ui/core';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-import { blue, green } from "@material-ui/core/colors";
+import { blue, green } from '@material-ui/core/colors';
 
 import { RouteWithSubRoutes } from './components';
-import { configureStore } from './redux/store';
+import configureStore from './redux/store';
 
 import routes from './routes';
 
@@ -14,24 +14,25 @@ const theme = createMuiTheme({
   palette: {
     primary: {
       light: blue[300],
-      main: "#027ACD",
-      dark: blue[700]
+      main: '#027ACD',
+      dark: blue[700],
     },
     secondary: {
       light: green[300],
-      main: green[500],
-      dark: green[700]
-    }
+      main: '#FCC201',
+      dark: green[700],
+    },
   },
   typography: {
-    useNextVariants: true
+    useNextVariants: true,
   },
-  shape: { borderRadius: 4 }
+  shape: { borderRadius: 4 },
 });
 
 const store = configureStore();
 
-const App = ({ classes }) => (
+const App = () => (
+  // eslint-disable-next-line
   <Provider store={store}>
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
