d468ffcf8c43b204c6faf2b1a253f9da58a4285f
Hugo <dummy@mio.com>
2018-12-07 13:00:28 -0600

    chore: Linting

--- src/components/Drawer.jsx
+++ src/components/Drawer.jsx
@@ -1,23 +1,23 @@
-import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-import { withRouter } from "react-router";
-import { withStyles } from "@material-ui/core/styles";
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router';
+import { withStyles } from '@material-ui/core/styles';
 import {
   List,
   ListItem,
   ListItemIcon,
   ListItemText,
-  Drawer
-} from "@material-ui/core";
+  Drawer,
+} from '@material-ui/core';
 import {
   Home,
   PieChart,
   ShoppingCart,
-} from "@material-ui/icons";
+} from '@material-ui/icons';
 import { Icon } from 'react-icons-kit';
 import { microchip } from 'react-icons-kit/fa/microchip';
 
-import { PATHS } from "../routes";
+import { PATHS } from '../routes';
 
 const DRAWER_WIDTH = 240;
 
@@ -46,37 +46,38 @@ class MyDrawer extends PureComponent {
           onClose={onClose}
           variant="temporary"
           classes={{ paper: classes.drawerPaper }}
-          ModalProps={{ keepMounted: true }}>
-            <List>
-              <ListItem
-                button
-                selected={this.activeRoute(PATHS.HOME)}
-                onClick={this.navigateToScreen(PATHS.HOME)}
-              >
-                <ListItemIcon><Home /></ListItemIcon>
-                <ListItemText primary="Home" />
-              </ListItem>
-              <ListItem
-                button
-                selected={this.activeRoute(PATHS.PLOTS)}
-                onClick={this.navigateToScreen(PATHS.PLOTS)}
-              >
-                <ListItemIcon><PieChart /></ListItemIcon>
-                <ListItemText primary="Charts" />
-              </ListItem>
-              <ListItem button>
-                <ListItemIcon><Icon icon={microchip} /></ListItemIcon>
-                <ListItemText primary="Web GL" />
-              </ListItem>
-              <ListItem
-                button
-                selected={this.activeRoute(PATHS.TEST_APP_1_HOME)}
-                onClick={this.navigateToScreen(PATHS.TEST_APP_1_HOME)}
-              >
-                <ListItemIcon><ShoppingCart /></ListItemIcon>
-                <ListItemText primary="Test App" />
-              </ListItem>
-            </List>
+          ModalProps={{ keepMounted: true }}
+        >
+          <List>
+            <ListItem
+              button
+              selected={this.activeRoute(PATHS.HOME)}
+              onClick={this.navigateToScreen(PATHS.HOME)}
+            >
+              <ListItemIcon><Home /></ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItem>
+            <ListItem
+              button
+              selected={this.activeRoute(PATHS.PLOTS)}
+              onClick={this.navigateToScreen(PATHS.PLOTS)}
+            >
+              <ListItemIcon><PieChart /></ListItemIcon>
+              <ListItemText primary="Charts" />
+            </ListItem>
+            <ListItem button>
+              <ListItemIcon><Icon icon={microchip} /></ListItemIcon>
+              <ListItemText primary="Web GL" />
+            </ListItem>
+            <ListItem
+              button
+              selected={this.activeRoute(PATHS.TEST_APP_1_HOME)}
+              onClick={this.navigateToScreen(PATHS.TEST_APP_1_HOME)}
+            >
+              <ListItemIcon><ShoppingCart /></ListItemIcon>
+              <ListItemText primary="Test App" />
+            </ListItem>
+          </List>
         </Drawer>
       </nav>
     );
@@ -84,21 +85,25 @@ class MyDrawer extends PureComponent {
 }
 
 MyDrawer.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.shape({}).isRequired,
+  onClose: PropTypes.func.isRequired,
+  anchor: PropTypes.string.isRequired,
+  open: PropTypes.bool.isRequired,
+  history: PropTypes.shape({}).isRequired,
 };
 
 const styles = theme => ({
-  root: { display: "flex" },
+  root: { display: 'flex' },
   drawer: {
     width: DRAWER_WIDTH,
-    flexShrink: 0
+    flexShrink: 0,
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing.unit * 3
+    padding: theme.spacing.unit * 3,
   },
   toolbar: theme.mixins.toolbar,
-  drawerPaper: { width: DRAWER_WIDTH }
+  drawerPaper: { width: DRAWER_WIDTH },
 });
 
 export default withRouter(withStyles(styles)(MyDrawer));
