ef1e333d122e8c4d140106aae63067e6b5a0e9af
Hugo <dummy@mio.com>
2018-12-10 12:19:50 -0600

    feat: New horizontal list component implemented

--- src/screens/TestApp1/Nav/Dashboard/CompetitorsList/CompetitorsList.jsx
+++ src/screens/TestApp1/Nav/Dashboard/CompetitorsList/CompetitorsList.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ScrollMenu from 'react-horizontal-scrolling-menu';
 import { withStyles } from '@material-ui/core/styles';
 import {
-  List,
   Typography,
   Card,
   CardMedia,
@@ -11,28 +11,18 @@ import {
   IconButton,
   Button,
 } from '@material-ui/core';
-import { Search, AddCircleOutline } from '@material-ui/icons';
+import {
+  Search,
+  AddCircleOutline,
+  ArrowBack,
+  ArrowForward,
+} from '@material-ui/icons';
 
 import './CompetitorsList.css';
 
 const styles = theme => ({
-  cardContent: {
-    minWidth: 250,
-    width: 250,
-    minHeight: 250,
-    height: 250,
-    marginRight: theme.spacing.unit * 4,
-  },
   media: {
     height: 150,
-    paddingTop: '56.25%', // 16:9
-  },
-  mediaNew: {
-    height: 150,
-    backgroundColor: '#EBEBEB',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
   },
   actions: {
     height: '50%',
@@ -41,10 +31,21 @@ const styles = theme => ({
     justifyContent: 'space-between',
     alignItems: 'space-between',
   },
+  cardContent: {
+    minWidth: 250,
+    width: 250,
+    minHeight: 250,
+    margin: theme.spacing.unit * 4,
+  },
+  content: {
+    flexDirection: 'column',
+    paddingBottom: 0,
+  },
 });
 
-const AddNewCard = ({ classes }) => (
-  <Card className={classes.cardContent}>
+/*
+const AddNewCard = (classes, key) => (
+  <Card className={classes.cardContent} key={key}>
     <Typography variant="h2" className={classes.mediaNew}>
       <AddCircleOutline fontSize="inherit" />
     </Typography>
@@ -60,34 +61,54 @@ const AddNewCard = ({ classes }) => (
     </CardActions>
   </Card>
 );
+*/
 
-const CompetitorsList = ({ competitorsList, classes }) => (
-  <div>
-    <Typography variant="h5" color="inherit">
-      My Competitors
-    </Typography>
-    <List className="list">
-      {competitorsList.map(comp => (
-        <Card className={classes.cardContent} key={comp.compName}>
-          <CardMedia
-            className={classes.media}
-            image={`https://picsum.photos/480/640?image=${Math.floor(Math.random() * 200)}`}
-            title="Paella dish"
-          />
-          <CardContent className={classes.actions}>
-            <Typography gutterBottom variant="h6">
-              {comp.compName}
-            </Typography>
-            <IconButton aria-label="Search">
-              <Search />
-            </IconButton>
-          </CardContent>
-        </Card>
-      ))}
-      <AddNewCard classes={classes} />
-    </List>
-  </div>
-);
+const ListComponent = (list, classes) => list.map(comp => (
+  <Card className={classes.cardContent} key={comp.compName}>
+    <CardMedia
+      className={classes.media}
+      image={`https://picsum.photos/480/640?image=${Math.floor(Math.random() * 200)}`}
+      title="Paella dish"
+    />
+    <CardContent className={classes.content}>
+      <Typography component="h5" variant="h5" className="wrap-text">
+        {comp.compName}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" color="primary">
+        Edit Info
+      </Button>
+      <IconButton aria-label="Search">
+        <Search />
+      </IconButton>
+    </CardActions>
+  </Card>
+));
+
+const CompetitorsList = ({ competitorsList, classes }) => {
+  const data = ListComponent(competitorsList, classes);
+  // data.push(AddNewCard(classes, 'New'));
+  return (
+    <div>
+      <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+        <Typography variant="h5" color="inherit">
+          My Competitors
+        </Typography>
+        <Button color="primary" variant="contained" style={{ marginLeft: 16 }}>
+          + Add New
+        </Button>
+      </div>
+      <ScrollMenu
+        data={data}
+        transition={0.3}
+        arrowLeft={<ArrowBack fontSize="large" />}
+        arrowRight={<ArrowForward fontSize="large" />}
+        onSelect={null}
+      />
+    </div>
+  );
+};
 
 CompetitorsList.propTypes = {
   competitorsList: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
