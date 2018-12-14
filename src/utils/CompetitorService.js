c2035ad69dea0b995a23779ec2e1a170604d653c
Hugo <dummy@mio.com>
2018-12-14 09:03:05 -0600

    feat: Data read from JSON

--- src/utils/CompetitorService.js
+++ src/utils/CompetitorService.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
+import dummy from './JsonGenerator/Competitors.json';
+
 function getCompetitorsList() {
+  return dummy;
+  /*
   return axios({
     method: 'get',
     url: 'https://www.mocky.io/v2/5c0952d12f00007d41637da2',
-    timeout: 4000,
+    timeout: 8000,
   })
     .then(res => {
       if (res.status === 200) return res.data;
       return undefined;
     });
+  */
 }
 
 export default {
--- src/utils/ShoppingListsService.js
+++ src/utils/ShoppingListsService.js
@@ -1,15 +1,20 @@
 import axios from 'axios';
 
+import dummy from './JsonGenerator/Shoppinglists.json';
+
 function getShoppinglistsList() {
+  return dummy;
+  /*
   return axios({
     method: 'get',
     url: 'https://www.mocky.io/v2/5c0965e12f00007b38637e18',
-    timeout: 4000,
+    timeout: 8000,
   })
     .then(res => {
       if (res.status === 200) return res.data;
       return undefined;
     });
+  */
 }
 
 export default {
