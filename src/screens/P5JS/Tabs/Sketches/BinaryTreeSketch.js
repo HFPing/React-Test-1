dc4dfdfb83d692c4713b7e2143c0f7614f0267ab
Hugo <dummy@mio.com>
2019-01-21 15:20:30 -0600

    feat: Graph sketch implemented

--- src/screens/P5JS/Tabs/Sketches/BinaryTreeSketch.js
+++ src/screens/P5JS/Tabs/Sketches/BinaryTreeSketch.js
@@ -1,23 +1,39 @@
-function Node(val) {
+function Node(val, x, y) {
   this.value = val;
   this.left = null;
   this.right = null;
+  this.x = x;
+  this.y = y;
 }
 
 Node.prototype.addNode = function (n) {
   if (n.value < this.value) {
-    if (this.left === null) this.left = n;
-    else this.left.addNode(n);
+    if (this.left === null) {
+      this.left = n;
+      this.left.x = this.x - 50;
+      this.left.y = this.y + 20;
+    } else this.left.addNode(n);
   } else if (n.value > this.value) {
-    if (this.right === null) this.right = n;
-    else this.right.addNode(n);
+    if (this.right === null) {
+      this.right = n;
+      this.right.x = this.x + 50;
+      this.right.y = this.y + 20;
+    } else this.right.addNode(n);
   }
 };
 
-Node.prototype.visit = function () {
-  if (this.left !== null) this.left.visit();
+Node.prototype.visit = function (p, parent) {
+  if (this.left !== null) this.left.visit(p, this);
   console.log(this.value);
-  if (this.right !== null) this.right.visit();
+  p.fill(255);
+  p.noStroke();
+  p.textAlign(p.CENTER);
+  p.text(this.value, this.x, this.y);
+  p.stroke(255);
+  p.noFill();
+  p.ellipse(this.x, this.y, 20, 20);
+  p.line(parent.x, parent.y, this.x, this.y);
+  if (this.right !== null) this.right.visit(p, this);
 };
 
 Node.prototype.search = function (val) {
@@ -31,14 +47,17 @@ function Tree() {
   this.root = null;
 }
 
-Tree.prototype.addValue = function (val) {
+Tree.prototype.addValue = function (val, p) {
   const n = new Node(val);
-  if (this.root === null) this.root = n;
-  else this.root.addNode(n);
+  if (this.root === null) {
+    this.root = n;
+    this.root.x = p.width / 2;
+    this.root.y = 16;
+  } else this.root.addNode(n);
 };
 
-Tree.prototype.traverse = function () {
-  this.root.visit();
+Tree.prototype.traverse = function (p) {
+  this.root.visit(p, this.root);
 };
 
 Tree.prototype.search = function (val) {
@@ -52,12 +71,13 @@ let tree;
 const sketch = p => {
   // eslint-disable-next-line no-param-reassign
   p.setup = () => {
-    p.noCanvas();
+    p.createCanvas(600, 400);
+    p.background(51);
     tree = new Tree();
     for (let i = 0; i < 10; i++) {
-      tree.addValue(Math.floor(p.random(0, 100)));
+      tree.addValue(Math.floor(p.random(0, 100)), p);
     }
-    tree.traverse();
+    tree.traverse(p);
     console.log(tree);
   };
   // eslint-disable-next-line no-param-reassign
